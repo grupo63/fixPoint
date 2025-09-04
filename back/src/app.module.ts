a2ff138a7484c1b671094846152c41b3cd3d7968
MFlorenciaGimenez <florgimenez14@hotmail.com>
2025-09-04 13:17:41 -0300

    configuracion inicial, conexion bdd con typeOrm

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from './config/typeorm';
+import { DataSourceOptions } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): DataSourceOptions => {
+        return configService.get<DataSourceOptions>('typeorm')!;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
