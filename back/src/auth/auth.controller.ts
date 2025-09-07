07466fed4c5d0cd99ec1fdec33357e154a40d968
MFlorenciaGimenez <florgimenez14@hotmail.com>
2025-09-07 19:38:15 -0300

    correccion de errores menores

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -34,24 +34,3 @@ export class AuthController {
     return this.authService.signIn(email, password);
   }
 }
-=======
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto, LoginUserDto } from './dto/auth.dto';
-import { AuthService } from './auth.service';
-
-@Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post('signup')
-  async signUp(@Body() user: CreateUserDto) {
-    return this.authService.signUp(user);
-  }
-
-  @Post('signin')
-  signIn(@Body() credentials: LoginUserDto) {
-    const { email, password } = credentials;
-    return this.authService.signIn(email, password);
-  }
-}
-
