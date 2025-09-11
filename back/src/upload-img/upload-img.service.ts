71c4a937b791a66764b7585ee752706de9645665
Tomás <tomascooklastra@gmail.com>
2025-09-11 11:10:59 -0300

    upload-img users

--- back/src/upload-img/upload-img.service.ts
+++ back/src/upload-img/upload-img.service.ts
@@ -4,6 +4,7 @@ import { UploadImgRepository } from './upload-img.repository';
 import { Repository } from 'typeorm';
 import { Professional } from 'src/professional/entity/professional.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class UploadImgService {
@@ -11,6 +12,8 @@ export class UploadImgService {
     private readonly uploadImgRepository: UploadImgRepository,
     @InjectRepository(Professional)
     private readonly professionalRepo: Repository<Professional>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
   ) {}
 
   async uploadProfileImg(file: Express.Multer.File, professionalId: string) {
@@ -29,6 +32,25 @@ export class UploadImgService {
 
     professional.profileImg = response.secure_url;
     await this.professionalRepo.save(professional);
-    return professional; // ← el front puede leer la URL directa
+    return professional;
+  }
+
+  async uploadUserProfileImg(file: Express.Multer.File, userId: string) {
+    const user = await this.userRepo.findOne({
+      where: { id: userId },
+    });
+    if (!user) throw new NotFoundException('User not found');
+
+    const response = await this.uploadImgRepository.uploadUserImage(
+      file,
+      `users/${userId}/profile`,
+    );
+    if (!response.secure_url)
+      throw new NotFoundException('Could not upload the image');
+
+    user.profileImage = response.secure_url;
+    await this.userRepo.save(user);
+
+    return user;
   }
 }
