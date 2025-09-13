23ae20e0ad688372db2d5c142aa8a75320668a83
MFlorenciaGimenez <florgimenez14@hotmail.com>
2025-09-13 06:15:24 -0300

    cambios menores

--- front/src/app/(views)/(publics)/professionals/page.tsx
+++ front/src/app/(views)/(publics)/professionals/page.tsx
@@ -4,7 +4,7 @@ import { Professional } from "@/types/profesionalTypes";
 
 export default async function ProfessionalsPage() {
   const profesionals = await fetchProfessionals();
-console.log(profesionals)
+  console.log(profesionals);
   return (
     <main className="p-6 space-y-8">
       {/* CABECERA ESTILIZADA */}
@@ -13,15 +13,20 @@ console.log(profesionals)
           Conectá con el profesional ideal
         </h1>
         <p className="text-blue-700 mt-2 text-base md:text-lg">
-          Encontrá plomeros, electricistas, carpinteros y más — todos verificados y listos para ayudarte.
+          Encontrá plomeros, electricistas, carpinteros y más — todos
+          verificados y listos para ayudarte.
         </p>
       </div>
 
       {/* GRID DE PROFESIONALES */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {profesionals.map((p: Professional) => (
-         <ProfessionalCard key={profesionals.id} pro={p} />
-        ))}
+        {Array.isArray(profesionals.data) && profesionals.data.length > 0 ? (
+          profesionals.data.map((p: Professional) => (
+            <ProfessionalCard key={p.id} pro={p} />
+          ))
+        ) : (
+          <p>No hay profesionales disponibles</p>
+        )}
       </div>
     </main>
   );
